1984d6defb0d21855e67e642f05c936af0922f41
Luis <luisjavierbetancourt@gmail.com>
2016-07-20 11:58:25 -0400

    Updated dashboard

--- app/project1/js/dashboard.js
+++ app/project1/js/dashboard.js
@@ -3,9 +3,12 @@ loadComponents([
       name: './components/dashboard.html',
     },
     {
+      name: './components/nav.html',
+    },
+    {
       name: './components/carousel.html',
       id: 'carousel'
-      },
+    },
     {
       name: './components/grids.html',
       id: 'carousel'
