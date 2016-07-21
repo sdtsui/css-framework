7ea72a460f97595866d2c7451aa8e50b34d22e8a
Luis <luisjavierbetancourt@gmail.com>
2016-07-21 02:04:48 -0400

    added express server to compile sass file when the user changes the scss variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,52 @@
-//write the project name here
-var activeProject = 'project1';
-var gulp = require('gulp');
+'use strict';
+
+// Write the project name here
+const activeProject = 'project1';
+
+// Dependencies
+const gulp = require('gulp');
 const babel = require('gulp-babel');
-var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var reload = browserSync.reload;
-const autoprefixer = require('gulp-autoprefixer');    // Prefixes the CSS
-const csscomb = require('gulp-csscomb');              // Formats the css file
-                                                      // according to the
-                                                      // csscomb.json file
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const csscomb = require('gulp-csscomb');
+const nodemon = require('gulp-nodemon');
 
-var project = './app/' + activeProject + '/';
-var cssDir = project + 'css/';
-var scssDir = project + 'scss/';
-var jsDir = project + 'js/';
+// Variables
+const reload = browserSync.reload;
+const cssCombConfigFile = './csscomb.json'; // css linter
+const prefixQuery = ['Firefox >= 15', 'IE >= 9', 'Chrome >= 25', 'iOS >= 5', 'Safari >= 7'];
 
-var files = {
+// Directory files
+const project = './app/' + activeProject + '/';
+const cssDir = project + 'css/';
+const scssDir = project + 'scss/';
+const jsDir = project + 'js/';
+const serverDir = './server/';
+
+// Watch these files
+const files = {
   scss: scssDir + '**/*.scss',
   html: project + '*.html',
-  js: project + 'js/*.js',
+  js: [serverDir + '*.js', jsDir + '*.js'],
   components: project + 'components/*.html'
 };
 
-var cssCombConfigFile = './csscomb.json';
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'babel'], function () {
-
-  browserSync({
-    server: project
-  });
-
-  gulp.watch(files.scss, ['sass']);
-  gulp.watch(files.html).on('change', reload);
-  gulp.watch(files.components, reload);
+// browsersync  Server + file watch + js transpilation + express server
+gulp.task('serve', ['nodemon', 'sass', 'babel'], () => {
+  browserSync({ server: project });
   gulp.watch(files.js, ['js-watch']);
+  gulp.watch(files.components, reload);
+  gulp.watch(files.html).on('change', reload);
+  gulp.watch(files.scss, ['sass']);
 });
 
 // Compile sass into CSS
-gulp.task('sass', function () {
+gulp.task('sass', () => {
   return gulp.src(scssDir + 'styles.scss')
              .pipe(sass().on('error', sass.logError))
              .pipe(autoprefixer({
-               browsers: [
-                 'Firefox >= 15',
-                 'IE >= 9',
-                 'Chrome >= 25',
-                 'iOS >= 5',
-                 'Safari >= 7'
-               ],
+               browsers: prefixQuery,
                cascade: false,
                add: true
              }))
@@ -59,9 +57,21 @@ gulp.task('sass', function () {
 
 // Compile JS to ES6
 gulp.task('babel', () => {
-  return gulp.src(jsDir + '*.js')
+  return gulp.src([jsDir + '*.js', serverDir + '*.js'])
              .pipe(babel({ presets: ['es2015'] }))
              .pipe(gulp.dest(project + 'dist/js/'));
+});
+
+// Start the express server
+gulp.task('nodemon', (cb) => {
+  let started = false;
+  return nodemon({ script: project + 'dist/js/server.js' }).on(['start'], () => {
+    // to avoid nodemon being started multiple times
+    if (!started) {
+      cb();
+      started = true;
+    }
+  });
 });
 
 gulp.task('js-watch', ['babel'], browserSync.reload);
--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const destination = './app/project1/scss/settings/user.scss';
 
-app.get('/test', (req, res) => {
-  res.send('test');
+// Get the request that sets the variables
+app.post('/compile', (req, res) => {
+  const newData = req.body;
+
+  // create an empty file
+  fs.writeFile('./app/project1/scss/settings/user.scss', '', function (err) {
+    if (err)return console.log(err);
+  });
+
+  // appends the new data to the file
+  const compileVariables = (sassFile) => {
+    Object.keys(sassFile).map(variable => {
+      const content = `${variable}: ${sassFile[variable]};\n`;
+
+      fs.appendFile(destination, content, (err) => {
+        if (err)return console.log(err);
+      });
+    });
+  };
+
+  compileVariables(newData);
+  res.send('compiled');
+
 });
 
 app.listen(3010, () => {
